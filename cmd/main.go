a2c7625bac27286459483e37c5f36dead96419be
Kamil <kdnuriev@gmail.com>
2024-06-03 19:06:21 +0400

    added gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"log"
-
-	"github.com/celestix/gotgproto"
-	"github.com/celestix/gotgproto/sessionMaker"
-	"github.com/glebarez/sqlite"
+	"github.com/go-vgo/robotgo"
 )
 
 func main() {
-	client, err := gotgproto.NewClient(
-		// Get AppID from https://my.telegram.org/apps
-		21326414,
-		// Get ApiHash from https://my.telegram.org/apps
-		"311562b87b3326cb20a864529520a121",
-		// ClientType, as we defined above
-		gotgproto.ClientTypePhone("+995557627581"),
-		// Optional parameters of client
-		&gotgproto.ClientOpts{
-			Session: sessionMaker.SqlSession(sqlite.Open("reminderbot")),
-		},
-	)
-	if err != nil {
-		log.Fatalln("failed to start client:", err)
-	}
-	if err := client.Idle(); err != nil {
-		log.Fatalln(err)
-	}
+	robotgo.MouseSleep = 100
 
+	robotgo.Move(700, 870)
+
+	robotgo.Click()
+
+	robotgo.TypeStr("Ты должен мне миллион рублей")
+
+	robotgo.KeyTap("enter")
 }
